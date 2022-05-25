7684a2b0f83101154bafe16fd5d0b616a1d590f0
NoahFreising <noah@freising.ch>
2022-05-25 15:47:58 +0200

    Add single page retrieval, previous/next functionality, Refactor

--- zdf_teletext.rb
+++ zdf_teletext.rb
@@ -3,8 +3,52 @@ require 'net/http'
 require 'nokogiri'
 require 'formatador'
 
+##
+# Class for accessing the ZDF Teletext
 class ZDFTeletext
-  # TODO add possibility to only retrieve a single page
+
+  ##
+  # Print a single page from the Teletext
+  def print_page(number)
+    teletext_request(number)
+  end
+
+  ##
+  # Runs the tool interactively, allowing easy browsing
+  def interactive_run()
+    # TODO add Date and Time to header
+    puts "ZDF-Teletext\n"
+    teletext_request('100')
+
+    while true do
+      puts "\t#{@previous_page} <-\t\t#{@current_page}\t\t -> #{@next_page}"
+      puts "Seite, (q)uit, (n)ext page, (p)revious page: " # TODO add next/previous pages
+      input = gets.chomp
+      
+      system "clear"
+      puts "ZDF-Teletext\n"
+      begin
+        case input
+          when "q"
+            break
+          when "p"
+            previous_page
+          when "n"
+            next_page
+          else
+            teletext_request(input)
+        end
+      rescue Exception => ex
+        puts ex.message
+        puts "Please enter a valid page, e.g. 100"
+      end
+    end
+  end
+
+  private
+
+  ##
+  # Removes unneccessary code from the html response
   def cleanup_body(html)
     html.css("head").remove
     html.css("div.breadcrumbs").remove
@@ -12,6 +56,8 @@ class ZDFTeletext
     return html.css("body")
   end
 
+  ##
+  # Checks if the html contains a table
   def check_table(html) # TODO maybe replace with "check_menu_table"?
     if html.at_css("table")
       true
@@ -20,7 +66,8 @@ class ZDFTeletext
     end
   end
 
-  # used to parse menus
+  ##
+  # Checks html for tables and prints them using formatador 
   def parse_table(html)
     if check_table(html)
       rows = Array.new
@@ -34,23 +81,29 @@ class ZDFTeletext
     end
   end
 
+  ##
+  # Makes a request to a given teletext site,
+  # sets instance variables and initiates printing
   def teletext_request(number)
     #make request
-    url = "https://teletext.zdf.de/teletext/zdf/seiten/"+number+".html"
+    url = "https://teletext.zdf.de/teletext/zdf/seiten/#{number}.html"
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri) # TODO add Timeout
     if(response.code == "200")
       html = Nokogiri::HTML(response.body)
-      @current_page=number
-      # TODO set @next_page and @previous_page by the attributes in the <body> tag
-      @next_page=html.css("body").map{ |body| body.attribute('next_page')}
-      @previous_page=html.css("body").map{ |body| body.attribute('previous_page')}
+
+      @current_page=html.css("body").attribute('page').value
+      @next_page=html.css("body").attribute('nextpg').value
+      @previous_page=html.at_css("body").attribute('prevpg').value
+
       teletext_print_response(html)
     else
       raise StandardError.new "Something went wrong trying to fetch the given page"
     end
   end
 
+  ##
+  # Prints the formatted Teletext to terminal
   def teletext_print_response(html)
     body = cleanup_body(html)
     if check_table(body)
@@ -60,43 +113,25 @@ class ZDFTeletext
     end
   end
 
+  ##
+  # Opens the previous page
   def previous_page
     teletext_request(@previous_page)
   end
 
+  ##
+  # Opens the next page
   def next_page
     teletext_request(@next_page)
-  end
-
-  def run()
-    # TODO add Date and Time to header
-    puts "ZDF-Teletext\n"
-    teletext_request('100')
-
-    while true do
-      puts "Seite, q zum Beenden: " # TODO add next/previous pages
-      input = gets.chomp
-      
-      system "clear"
-      puts "ZDF-Teletext\n"
-
-      case input
-        when "q"
-          break
-        when "p"
-          previous_page
-        when "n"
-          next_page
-        else # TODO durch case mit -INFINITY...INFINITY ersetzen
-          begin
-            teletext_request(input)
-          rescue Exception => ex
-            puts ex.message
-          end
-      end
-    end
   end
 end
 
 teletext = ZDFTeletext.new
-teletext.run
+
+if ARGV.length == 1
+  teletext.print_page(ARGV[0])
+elsif ARGV.length == 0
+  teletext.interactive_run
+else
+  raise StandardError.new("Usage: ruby zdf_teletext.rb [page no.]")
+end
